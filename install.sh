300032c4a892339c286e07b2c05749e42d7e8fd9
Nguyen Hop Quang <quangnh@sotanext.com>
2021-11-25 15:39:43 +0700

    update not use homebrew

--- install.sh
+++ install.sh
@@ -11,34 +11,28 @@ cat << "EOF"
 EOF
 
 echo "Removing old version of KikiLogin if installed..."
-rm -rf /Applications/KikiLogin.app
-echo "Installing KikiLogin....."
-if ! command -v brew &> /dev/null
-then
-    echo "Homebrew not found! Installing Homebrew"
-    echo -ne '\n' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    echo "Install Homebrew success!"
-    exit
-fi
-echo "Checking for NodeJS"
-if ! command -v node &> /dev/null
-then
-    echo "NodeJS not found! Installing NodeJS..."
-    brew install node
-    echo "Install NodeJS success!"
-    exit
-fi
-echo "Checking for KikiLogin dependencies..."
 echo "KikiLogin will be installed to $HOME/Documents/chrome-mac"
 rm -rf "$HOME/Documents/chrome-mac/"
 mkdir "$HOME/Documents/chrome-mac"
 cd "$HOME/Documents/chrome-mac/"
+rm -rf /Applications/KikiLogin.app
+echo "Installing KikiLogin....."
+echo "Checking for NodeJS"
+if ! command -v node &> /dev/null
+then
+    echo "NodeJS not found! Installing NodeJS..."
+    curl -L https://nodejs.org/download/release/v14.17.5/node-v14.17.5.pkg > nodejs.pkg
+    installer -pkg nodejs.pkg -target CurrentUserHomeDirectory
+    echo "Install NodeJS success!"
+fi
+echo "Checking for KikiLogin dependencies..."
 curl -L https://github.com/kikilogin/kikiinstaller-macos/releases/download/v1.0/kikilogin.zip > file.zip
 echo "Finished download KikiLogin. Extracting..."
 unzip -q file.zip
-rm file.zip
 echo "Installing KikiLogin dependencies..."
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm i &> /dev/null
 echo "Copy KikiLogin app to App folder...."
 cp -R KikiLogin.app /Applications/KikiLogin.app
+rm nodejs.pkg
+rm file.zip
 echo "Finished!"
